949ccd234980f60fa10cf2bbd3e5f7f6da2b4e7a
Dru Lang <dru@Drus-MacBook-Air.local>
2013-07-15 21:52:05 -0400

    Fixed person_list script. Added sql script

--- 2013/scripts/person_list_sql_gen.py
+++ 2013/scripts/person_list_sql_gen.py
@@ -8,9 +8,9 @@ f = open(sys.argv[1])
 
 for line in f:
     parts = line.split("`")
-    nbr = parts[0]
-    fname = parts[1]
-    lname = parts[2]
+    nbr = parts[0].strip()
+    fname = parts[1].strip().replace("'","")
+    lname = parts[2].strip().replace("'","")
 
     query = """insert into PERSON (ID, SHOW_EXHIBITOR_ID, FIRST_NAME, LAST_NAME, ADD_DATE) values (%s,'%s','%s','%s',NOW());""" % (nbr, nbr, fname,lname)
     reg_link_query = "insert into SHOW_REGISTRATION_LINK (SHOW_ID, PERSON_ID, SHOW_REGISTRATION_STATUS_ID, EXHIBITOR_ID, DATE_ADD) values (2,%s,1,'%s',NOW());" % (nbr,nbr)
