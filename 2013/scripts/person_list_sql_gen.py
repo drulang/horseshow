e1bbb1b9d184937dbedcd99eb6a513a3ba3c6bc6
Dru Lang <drulang@gmail.com>
2013-07-19 07:02:21 -0400

    Fixed ex number

--- 2013/scripts/person_list_sql_gen.py
+++ 2013/scripts/person_list_sql_gen.py
@@ -9,11 +9,20 @@ f = open(sys.argv[1])
 for line in f:
     parts = line.split("`")
     nbr = parts[0].strip()
+
+    if len(nbr) == 1:
+        str_nbr = "00" + nbr
+    elif len(nbr) == 2:
+        str_nbr = "0" + nbr
+    else:
+        str_nbr = nbr
+
+
     fname = parts[1].strip().replace("'","")
     lname = parts[2].strip().replace("'","")
 
-    query = """insert into PERSON (ID, SHOW_EXHIBITOR_ID, FIRST_NAME, LAST_NAME, ADD_DATE) values (%s,'%s','%s','%s',NOW());""" % (nbr, nbr, fname,lname)
-    reg_link_query = "insert into SHOW_REGISTRATION_LINK (SHOW_ID, PERSON_ID, SHOW_REGISTRATION_STATUS_ID, EXHIBITOR_ID, DATE_ADD) values (2,%s,1,'%s',NOW());" % (nbr,nbr)
+    query = """insert into PERSON (ID, SHOW_EXHIBITOR_ID, FIRST_NAME, LAST_NAME, ADD_DATE) values (%s,'%s','%s','%s',NOW());""" % (nbr, str_nbr, fname,lname)
+    reg_link_query = "insert into SHOW_REGISTRATION_LINK (SHOW_ID, PERSON_ID, SHOW_REGISTRATION_STATUS_ID, EXHIBITOR_ID, DATE_ADD) values (2,%s,1,'%s',NOW());" % (nbr,str_nbr)
     print query
     print reg_link_query
 
